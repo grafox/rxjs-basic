c67835a6e4a238487d0cc2258c57d1090cde1e53
Gassan <g_fox_fts@yahoo.com>
2020-09-18 21:31:13 +0200

    Observable

---
+++ 01.ts
@@ -0,0 +1,17 @@
+import { Observable } from 'rxjs';
+
+const observer = {
+    next: value => console.log('next', value),
+    error: error => console.log('error', error),
+    complete: () => console.log('complete!')
+}
+
+const observable = new Observable(subscriber => {
+    subscriber.next('Hello');
+    subscriber.next('World');
+    subscriber.complete();
+})
+
+observable.subscribe(
+    value => console.log('next', value)
+)
